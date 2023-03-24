1f4671e01af14928e4e40d62f8552708c513710a
JustinSixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-24 17:56:00 -0500

    2 files modified

--- src/cart.js
+++ src/cart.js
@@ -1,19 +1,19 @@
 function attachAddToCart() {
   /* Add your add to cart logic here */
+  let cart = [];
 
   // Element Selectors
   const addButton = Array.from(
     document.querySelectorAll(".add-to-cart-button")
   );
   const shoppingCart = document.getElementById("cart-size");
-  let qty = 0;
+  let cartQty = 0;
 
   // Event Listeners
   addButton.forEach((button) => {
     button.addEventListener("click", function () {
       button.textContent = "Added to Cart";
-      button.classList.add(".selected-btn");
-      qty++;
+      cartQty++;
       shoppingCart.textContent = qty;
     });
   });
